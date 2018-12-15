502e9c665a274975c8f8ab81870c7279559e79da
RodneyMorgan97 <rtm36@zips.uakron.edu>
2018-12-15 18:48:11 -0500

    add navbar animation

--- src/js/navbar.js
+++ src/js/navbar.js
@@ -8,4 +8,25 @@ $(document).ready(function() {
             $('.navbar').removeClass('sticky');
         }
     });
+
+    $("#nav ul li a[href^='#']").on('click', function(e) {
+        console.log('click');
+        // prevent default anchor click behavior
+        e.preventDefault();
+     
+        // store hash
+        var hash = this.hash;
+     
+        // animate
+        $('html, body').animate({
+            scrollTop: $(hash).offset().top
+          }, 500, function(){
+     
+            // when done, add hash to url
+            // (default click behaviour)
+            window.location.hash = hash;
+          });
+     
+     });
 });
+
